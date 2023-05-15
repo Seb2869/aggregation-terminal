dc3f281372f565738f36a66aa9ba86f38e670fe7
Oluwakorede Fashokun <koredefashokun@gmail.com>
2023-05-15 13:10:08 -0400

    fix: issue with static route generation (#6)

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 /** @type {import("next").NextConfig} */
 module.exports = withBundleAnalyzer({
+  trailingSlash: true,
   reactStrictMode: true,
   swcMinify: false,
   images: {
